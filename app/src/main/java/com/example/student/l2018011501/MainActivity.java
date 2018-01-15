0a77b10fdf011c5c601e29a6b02d0107e860fa2e
ericchiu <ericjack7@gmail.com>
2018-01-15 09:44:08 +0800

    Initial commit

---
+++ app/src/main/java/com/example/student/l2018011501/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.student.l2018011501;
+
+import android.graphics.Bitmap;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageRequest;
+import com.android.volley.toolbox.Volley;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void click1(View v)
+    {
+        RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+        ImageRequest request =new ImageRequest("http://campingouzouni.com/wp-content/uploads/2015/05/beach-volley.jpg",
+                new Response.Listener<Bitmap>() {
+                    @Override
+                    public void onResponse(Bitmap response) {
+                            ImageView img=findViewById(R.id.imageView);
+                            img.setImageBitmap(response);
+                    }
+                }, 0, 0, ImageView.ScaleType.FIT_XY, Bitmap.Config.RGB_565, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        queue.add(request);
+        queue.start();
+
+    }
+}
