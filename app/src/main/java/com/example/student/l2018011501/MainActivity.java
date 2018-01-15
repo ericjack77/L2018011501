a2f972ef1b11cf499ab1beddd49573fd277b66b1
ericchiu <ericjack7@gmail.com>
2018-01-15 10:47:13 +0800

    玩弄進度條  等抓到的時候就消失

--- app/src/main/java/com/example/student/l2018011501/MainActivity.java
+++ app/src/main/java/com/example/student/l2018011501/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -15,22 +16,27 @@ import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
-
+    ProgressBar pb;
+    ImageView img;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        img=findViewById(R.id.imageView);
+        pb=findViewById(R.id.progressBar);
     }
 
     public void click1(View v)
     {
+        pb.setVisibility(View.VISIBLE);
         RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
         ImageRequest request =new ImageRequest("http://campingouzouni.com/wp-content/uploads/2015/05/beach-volley.jpg",
                 new Response.Listener<Bitmap>() {
                     @Override
                     public void onResponse(Bitmap response) {
-                            ImageView img=findViewById(R.id.imageView);
                             img.setImageBitmap(response);
+                            pb.setVisibility(View.INVISIBLE);
+                            img.setVisibility(View.VISIBLE);
                     }
                 }, 0, 0, ImageView.ScaleType.FIT_XY, Bitmap.Config.RGB_565, new Response.ErrorListener() {
             @Override
